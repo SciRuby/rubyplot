21b3470ef646e57aba2c6c3fdb071587b92fc0e9
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2019-04-27 22:04:56 +0900

    plotting equidistant simple line errors on line plot

--- lib/rubyplot/artist/line2d.rb
+++ lib/rubyplot/artist/line2d.rb
@@ -2,19 +2,20 @@ module Rubyplot
   module Artist
     class Line2D < Artist::Base
       # rubocop:disable Metrics/ParameterLists
-      def initialize(owner, x:,y:,color: :black, opacity: 0.0, width: 1.0)
+      def initialize(owner, x:,y:,color: :black, opacity: 0.0, width: 1.0, type: :solid)
         @owner = owner
         @x = x
         @y = y
         @color = color
         @opacity = opacity
         @width = width
+        @type = type
       end
       # rubocop:enable Metrics/ParameterLists
 
       def draw
         Rubyplot.backend.draw_lines(x: @x, y: @y,
-          width: @width, color: @color, opacity: @opacity, type: :solid)
+          width: @width, color: @color, opacity: @opacity, type: @type)
       end
     end # class Line2D
   end # class Artist
--- lib/rubyplot/artist/plot/error_bar.rb
+++ lib/rubyplot/artist/plot/error_bar.rb
@@ -11,24 +11,79 @@ module Rubyplot
 
         def process_data
           super
-          size = @data[:y_values].size
-          @yuplims = Array.new(size) { nil } unless @yuplims
-          @ylolims = Array.new(size) { nil } unless @ylolims
-          @xuplims = Array.new(size) { nil } unless @xuplims
-          @xlolims = Array.new(size) { nil } unless @xlolims
-
+          
+          preprocess_err_values!
+          
           check_lims_sizes
           check_err_sizes
 
-          if @yerr
-          end
+          @line = Rubyplot::Artist::Line2D.new(
+            self,
+            x: @data[:x_values],
+            y: @data[:y_values],
+            color: @data[:color]
+          )
+          generate_yerr if @yerr
+          generate_xerr if @xerr
         end
 
         def draw
-
+          @line.draw
+          @yerr_lines.each(&:draw) if @yerr
+          @xerr_lines.each(&:draw) if @xerr
         end
 
         private
+
+        def preprocess_err_values!
+          if @yerr
+            if @yerr.is_a?(Float)
+              @yerr = [@yerr] * @data[:y_values].size
+            end
+          end
+
+          if @xerr
+            if @xerr.is_a?(Float)
+              @xerr = [@xerr] * @data[:x_values].size
+            end
+          end
+        end
+
+        def generate_xerr
+          @xerr_lines = @xerr.map.with_index do |xe, idx|
+            xcoord = @data[:x_values][idx]
+            ycoord = @data[:y_values][idx]
+
+            if !@xuplims && !@xlolims
+              Rubyplot::Artist::Line2D.new(
+                self,
+                x: [xcoord - xe, xcoord + xe],
+                y: [ycoord, ycoord],
+                color: @data[:color]
+              )
+            else
+              if @xuplims
+                
+              end
+            end
+          end          
+        end
+
+        def generate_yerr
+          @yerr_lines = @yerr.map.with_index do |ye, idx|
+            xcoord = @data[:x_values][idx]
+            ycoord = @data[:y_values][idx]
+
+            if !@yuplims && !@ylolims
+              Rubyplot::Artist::Line2D.new(
+                self,
+                x: [xcoord, xcoord],
+                y: [ycoord - ye, ycoord + ye],
+                color: @data[:color]
+              )
+            end
+          end
+        end
 
         def check_lims_sizes
           
