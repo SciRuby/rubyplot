7b587809fc0002598759ea124881e620e0797f3f
Alish Dipani <alish.dipani@gmail.com>
2019-07-04 06:15:14 +0530

    replaced border_opacity with fill_opacity

--- lib/rubyplot/artist/circle.rb
+++ lib/rubyplot/artist/circle.rb
@@ -2,14 +2,14 @@ module Rubyplot
   module Artist
     class Circle < Base
       # rubocop:disable Metrics/ParameterLists
-      def initialize(owner, x:, y:, radius:, border_opacity: 0.0,
-                     color: :default, border_width:, abs: false)
+      def initialize(owner, x:, y:, radius:, fill_opacity: 1.0,
+        color: :default, border_width:, abs: false)
         @x = x
         @y = y
         @owner = owner
         @radius = radius
         @border_width = border_width
-        @border_opacity = border_opacity
+        @fill_opacity = fill_opacity
         @color = color
         @abs = abs
       end
@@ -20,12 +20,11 @@ module Rubyplot
           x: @x,
           y: @y,
           radius: @radius,
-          #          border_opacity: @border_opacity,
           border_type: :solid,
           border_width: @border_width,
           fill_color: @color,
           border_color: @color,
-          fill_opacity: 1.0
+          fill_opacity: @fill_opacity
         )
       end
     end # class Circle
