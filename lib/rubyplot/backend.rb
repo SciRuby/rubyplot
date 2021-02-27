5e7030f603346048b18640142467d2d15924a463
Josep Egea <jes@josepegea.com>
2021-02-27 21:03:21 +0100

    Keep ImageMagick as default backend when no ENV variable present

--- lib/rubyplot/backend.rb
+++ lib/rubyplot/backend.rb
@@ -1,10 +1,10 @@
 require_relative 'backend/base'
 if ENV["RUBYPLOT_BACKEND"] == "GR"
   require_relative 'backend/gr_wrapper'
-elsif ENV["RUBYPLOT_BACKEND"] == "MAGICK"
-  require_relative 'backend/magick_wrapper'
 elsif ENV["RUBYPLOT_BACKEND"] == "TK_CANVAS"
   require_relative 'backend/tk_canvas_wrapper'
+elsif ENV["RUBYPLOT_BACKEND"] = "MAGICK"
+  require_relative 'backend/magick_wrapper'
 end
 
 
